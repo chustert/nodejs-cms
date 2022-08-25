4cb86f29e9dba3de50695592cb0f7d3ed9610fda
chustert <christianhustert@gmail.com>
2022-08-26 11:52:39 +1200

    displaying data back to the form

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -42,4 +42,10 @@ router.post('/create', (req, res) => {
     });
 });
 
+router.get('/edit/:id', (req, res) => {
+    Post.findOne({_id: req.params.id}).then(post => {
+        res.render('admin/posts/edit', {post: post});
+    }); 
+});
+
 module.exports = router;
