7f8723d0e0fc8024f903a91c02d2abfc75008134
chustert <christianhustert@gmail.com>
2022-08-26 11:04:52 +1200

    saving a post

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../../models/Post');
 
 router.all('/*', (req, res, next)=> {
     req.app.locals.layout = 'admin';
@@ -15,7 +16,28 @@ router.get('/create', (req, res) => {
 });
 
 router.post('/create', (req, res) => {
-    console.log(req.body);
+    let allowComments = true;
+    
+    if(req.body.allowComments) {
+        allowComments = true;
+    } else {
+        allowComments = false;
+    }
+
+    const newPost = new Post({
+        title: req.body.title,
+        status: req.body.status,
+        allowComments: allowComments,
+        body: req.body.body
+    });
+    // console.log(req.body);
+
+    newPost.save().then(savedPost => {
+        console.log(savedPost);
+        res.redirect('/admin/posts');
+    }).catch(error => {
+        console.log("could not save post");
+    });
 });
 
 module.exports = router;
